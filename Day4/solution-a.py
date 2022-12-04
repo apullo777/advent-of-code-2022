be64960cde07e0ee2bd1b58dea513cda24e57766
apullo777 <peter.yaochen.chang@gmail.com>
2022-12-04 18:29:50 +0100

    add comments

--- Day4/solution-a.py
+++ Day4/solution-a.py
@@ -20,6 +20,14 @@ def is_zero(x):
         return True
     return False
 
+# For two intervals A and B
+# A fully contains B when 
+# (a) A.lower <= B.lower and 
+# (b) A.upper >= B.upper 
+# so we test the difference of their lower and upper bounds
+# if the product != abs(product), the two values have different sign, meaning that one fully contain another
+# we also need to check whether they equal at one end, ex. (2, 5) and (3, 5) also count as full inclusion
+
 def is_fully_contain(interval_1, interval_2):
     lower_diff = int(lower_bound(interval_1)) - int(lower_bound(interval_2))
     upper_diff = int(upper_bound(interval_1)) - int(upper_bound(interval_2))
--- Day4/solution-b.py
+++ Day4/solution-b.py
@@ -21,6 +21,17 @@ def is_zero(x):
         return True
     return False
 
+# there are three major cases of two intervals 
+# 1. fully containing
+# 2. partially overlaping
+# 3. non-overlapping
+# Since we already knew how to find case 1 in Part 1, our goal is to distinguish the latter 2
+# In cases where full inclusion has been excluded, if a partial overlap occurs,
+# regardless of whether one interval is on the left or right of the other, 
+# the product of the differences between the upper and lower bounds of the two intervals,
+# should be different from its abosulte (different sgin or zero)
+# excluding these cases then we have non-overlapping
+
 def check_diff(interval_1, interval_2, mode):
     if mode == "parallel":
         diff_1 = int(lower_bound(interval_1)) - int(upper_bound(interval_2))
