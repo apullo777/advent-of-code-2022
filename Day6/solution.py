3e7f23afd877700c346f737686b21375412fa003
apullo777 <peter.yaochen.chang@gmail.com>
2022-12-06 19:46:27 +0100

    refactor using set

--- Day6/solution.py
+++ Day6/solution.py
@@ -1,19 +1,10 @@
 with open('input.txt') as file:
     data = file.read().strip()
 
-def char_index(char):
-    return ord(char) - ord("a")
-
 def check(start_index, signal, marker_amount):
-    frequencies = [0] * 26
-    for char in signal:
-        frequencies[char_index(char)] += 1
-    
-    nonzero_count = sum(1 for freq in frequencies if (freq != 0))
-
-    if nonzero_count == marker_amount: 
-        return start_index + marker_amount
-    else: return 0
+    if len(set(signal)) != len(signal):
+        return 0
+    return start_index + marker_amount
 
 def iterate(signal, marker_amount):
     index = 0
@@ -24,5 +15,3 @@ def iterate(signal, marker_amount):
 
 print(iterate(data, 4)) # part 1
 print(iterate(data, 14)) # part 2
-
-
