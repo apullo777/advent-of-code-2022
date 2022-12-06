737bf9256f4208815b9066f1506dc9eed865b8d5
apullo777 <peter.yaochen.chang@gmail.com>
2022-12-06 22:52:39 +0100

    add comment

--- Day6/solution.py
+++ Day6/solution.py
@@ -1,17 +1,17 @@
 with open('input.txt') as file:
     data = file.read().strip()
 
-def check(start_index, signal, marker_amount):
+def check(start_index, signal, marker_len):
     if len(set(signal)) != len(signal):
         return 0
-    return start_index + marker_amount
+    return start_index + marker_len
 
-def iterate(signal, marker_amount):
+def iterate(signal, marker_len):
     index = 0
-    for i in range(len(signal)-3):
-        index += check(i, signal[i:i+marker_amount], marker_amount)
+    for i in range(len(signal) - marker_len + 1):
+        index += check(i, signal[i:i+marker_len], marker_len)
         if index: break
     return index
 
-print(iterate(data, 4)) # part 1
-print(iterate(data, 14)) # part 2
+print(iterate(data, 4)) # part 1: packet
+print(iterate(data, 14)) # part 2: message
