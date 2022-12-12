ae2ad107f51481bdef1ab0dfa947f1367ea27e39
apullo777 <peter.yaochen.chang@gmail.com>
2022-12-13 00:44:30 +0100

    add Day11

---
+++ Day11/solution.py
@@ -0,0 +1,79 @@
+import math
+
+with open('test.txt') as file:
+    data = file.read().split("\n\n")
+
+# Parse instructions for each monkey
+monkeys = []
+for monkey in data:
+    lines =  monkey.split("\n")
+
+    starting_items_line = lines[1].strip().split(": ")[1]
+    starting_items = [int(x) for x in starting_items_line.split(",")]
+
+    operation_line = lines[2].strip()
+    operation = operation_line.split("=")[1].strip()
+
+    test_line = lines[3].strip()
+    modulo = test_line.split("by")[1].strip()
+
+    true_branch_line = lines[4].strip()
+    true_branch = int(true_branch_line.split(" ")[-1])
+
+    false_branch_line = lines[5].strip()
+    false_branch = int(false_branch_line.split(" ")[-1])
+
+    monkey = {
+        "items": starting_items,
+        "do": operation,
+        "mod": int(modulo),
+        "true": int(true_branch),
+        "false": int(false_branch),
+        "count": 0
+    }
+    monkeys.append(monkey)
+
+op = {'+': lambda x, y: x + y,
+      '-': lambda x, y: x - y,
+      '*': lambda x, y: x * y}
+
+def do_parser(do, item):
+    operand_1, operator, operand_2 = do.strip().split(" ")
+    if operand_2.isnumeric():
+        return op[operator](int(item), int(operand_2))
+    if operand_2 == 'old':
+        return op[operator](int(item), int(item))
+
+print(do_parser('old * old', 79)//3%13)
+
+def throw_item(monkeys, item, index):
+    item = item // 3
+    print(item)
+    monkeys[index]['items'].append(item)
+
+def play_round(monkeys):
+    for i in range(len(monkeys)):
+        while(monkeys[i]['items']):
+            monkeys[i]['count'] += 1
+            monkeys[i]['items'][0] = do_parser(monkeys[i]['do'], monkeys[i]['items'][0])
+            if monkeys[i]['items'][0] % monkeys[i]['mod']:
+                throw_item(monkeys, monkeys[i]['items'].pop(0), monkeys[i]['false'])
+            else: 
+                throw_item(monkeys, monkeys[i]['items'].pop(0), monkeys[i]['true'])
+
+print(monkeys[2]['items'][0])
+
+# Part 1:
+for _ in range(20):
+    item = []
+    play_round(monkeys)
+    for monkey in monkeys:
+        item.append(monkey['items'])
+    print(item)
+
+result_1 = []
+for monkey in monkeys:
+    result_1.append(monkey['count'])
+
+print(result_1)
+print(math.prod(sorted(result_1)[-2:]))
